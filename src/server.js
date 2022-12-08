8d29020cb1d0927277a3334124d5f919c914f965
FelipeMMoraes <felipe.moraes8@hotmail.com>
2022-12-08 17:21:40 -0300

    initial files

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.post("/users", (request, response) => {
+  response.send("Voce chamou o POST")
+})
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
